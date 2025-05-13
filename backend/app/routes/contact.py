0a6de90e0bf021f9373577b4f2d057722327d7ae
Sai Sweth Koormachalam <119130522+codewithsweth@users.noreply.github.com>
2025-05-14 02:29:49 +0530

    Added logic for post contact api

--- backend/app/routes/contact.py
+++ backend/app/routes/contact.py
@@ -1,3 +1,9 @@
 from fastapi import APIRouter
+from app.schemas.contact import ContactMessage
 
 router = APIRouter(prefix="/contact", tags=["Contact"])
+
+@router.post("/")
+def send_message(contact: ContactMessage):
+    print(f"New contact from <{contact.email}>: {contact.message}")
+    return {"message": 'Message received!, Thank you!'}
