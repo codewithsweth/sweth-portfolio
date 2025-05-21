0255a4f11e7127451f349c5f0d580f9e5645f438
Sai Sweth Koormachalam <119130522+codewithsweth@users.noreply.github.com>
2025-05-22 02:01:56 +0530

    1. Blog Details page - basic design. 2. Integrated with backend API. 3. Install style library

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+
 module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [typography],
 };
