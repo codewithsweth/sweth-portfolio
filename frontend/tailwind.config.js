f2c7d7fc0bb4a4015c38d5df010046ad69007469
Sai Sweth Koormachalam <119130522+codewithsweth@users.noreply.github.com>
2025-05-22 02:03:18 +0530

    Revert "1. Blog Details page - basic design. 2. Integrated with backend API. 3. Install style library"
    This reverts commit 0255a4f11e7127451f349c5f0d580f9e5645f438.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-import typography from '@tailwindcss/typography';
-
 module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [typography],
+  plugins: [],
 };
