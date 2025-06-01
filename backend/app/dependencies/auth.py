6b2d31e4e57fbeb9207ca2c92b6a72bf861f56bc
Sai Sweth Koormachalam <119130522+codewithsweth@users.noreply.github.com>
2025-06-01 16:56:38 +0530

    Resolved invalid token issue while creating a project

--- backend/app/dependencies/auth.py
+++ backend/app/dependencies/auth.py
@@ -6,7 +6,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def get_current_admin(token: str = Depends(oauth2_scheme)):
     try:
-        payload = jwt.decode(token, os.getenv("JWT_SECRET"), algorithms=[os.getenv("HWT_ALGORITHM")])
+        payload = jwt.decode(token, os.getenv("JWT_SECRET"), algorithms=[os.getenv("JWT_ALGORITHM")])
         email = payload.get("sub")
         if email != os.getenv("ADMIN_EMAIL"):
             raise HTTPException(status_code=401, detail="Invalid token")
